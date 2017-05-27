c18b6e8a248861cb089ba370a54e7e1a00e05652
Wziątek Grzegorz <=>
2017-05-27 11:16:05 +0200

    php...

--- backend/src/controllers.php
+++ backend/src/controllers.php
@@ -29,3 +29,7 @@ $app->error(function (\Exception $e, Request $request, $code) use ($app) {
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
 });
+
+
+require 'controllers/blog.php';
+require 'controllers/user.php';
