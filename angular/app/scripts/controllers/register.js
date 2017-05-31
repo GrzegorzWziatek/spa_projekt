ec373a83eef9a5814f391f7330f02bdb54fa50f6
kicur <artriks10@poczta.fm>
2017-05-31 20:58:47 +0200

    Added registration feature

--- angular/app/scripts/controllers/register.js
+++ angular/app/scripts/controllers/register.js
@@ -8,12 +8,30 @@
  * Controller of the carpoolingApp
  */
 angular.module('carpoolingApp')
-  .controller('RegisterCtrl', ['userService', '$location', function (userService, $location) {
-
+  .controller('RegisterCtrl', ['$scope','userService', 'registerService', '$location', function ($scope, userService, registerService, $location) {
   if (userService.isLogged()) {
       $location.path('/');
     }
+  $scope.validate = function(form) {
+    if (!form.$valid){
+      return;
+    }
 
+    var enteredData = {
+      email : $scope.email,
+      password: $scope.password,
+      login : $scope.login,
+      car : $scope.car,
+      plates: $scope.plates,
+      desc: $scope.desc
+    };
+    registerService.register(enteredData, function(logged){
+      if (logged){
+        $location.path('/');
+      }
+    });
+
+  };
 
 
 
