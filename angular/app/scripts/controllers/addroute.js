8857f13771fd6caef82000a5905e0334ff08a86b
Wziątek Grzegorz <=>
2017-06-05 18:57:05 +0200

    update

---
+++ angular/app/scripts/controllers/addroute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carpoolingApp.controller:AddrouteCtrl
+ * @description
+ * # AddrouteCtrl
+ * Controller of the carpoolingApp
+ */
+angular.module('carpoolingApp')
+  .controller('AddrouteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
