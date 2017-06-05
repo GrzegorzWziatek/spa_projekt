678e0605d2d022ff1c9d64261188fc3555472dde
Wziątek Grzegorz <=>
2017-06-05 18:54:59 +0200

    Merge branch 'master' of /Users/grzegorz/OneDrive/Uczelnie/UEK/SPA/spa_projekt with conflicts.

---
+++ angular/app/scripts/controllers/addblog.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carpoolingApp.controller:AddblogCtrl
+ * @description
+ * # AddblogCtrl
+ * Controller of the carpoolingApp
+ */
+angular.module('carpoolingApp')
+  .controller('AddblogCtrl', ['$scope', 'blogService', 'userService', '$location', function ($scope, blogService, userService, $location) {
+
+    $scope.validate = function(form) {
+      if (!form.$valid) {
+        return;
+      }
+
+      var enteredData = {
+        title : $scope.title,
+        text :  $scope.text
+      };
+
+      blogService.save(enteredData, function(saved) {
+        if (saved){
+          $location.path('/#blog');
+        }
+      });
+
+    };
+
+
+  }]);
--- angular/test/karma.conf.js
+++ angular/test/karma.conf.js
@@ -31,6 +31,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/trix/dist/trix.js',
+      'bower_components/angular-trix/dist/angular-trix.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
