1a7f1991b474b6336c017958e3ad88f5dd705348
kicur <artriks10@poczta.fm>
2017-06-07 19:06:29 +0200

    addroute.js functionality

--- angular/app/scripts/controllers/addroute.js
+++ angular/app/scripts/controllers/addroute.js
@@ -8,10 +8,30 @@
  * Controller of the carpoolingApp
  */
 angular.module('carpoolingApp')
-  .controller('AddrouteCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AddrouteCtrl', ['$scope', 'routesService', 'userService', '$location', function ($scope, routesService, userService, $location) {
+
+    $scope.validate = function (form) {
+      if (!form.$valid) {
+        return;
+      }
+
+      var enteredData = {
+        routeFrom: $scope.routeFrom,
+        routeTo: $scope.routeTo,
+        date: $scope.date,
+        maxPassengers: $scope.maxPassengers,
+        price: $scope.price,
+        text: $scope.text
+      };
+
+      routesService.save(enteredData, function (id) {
+        if (id) {
+          var path = '/route/' + id;
+          $location.path(path);
+        }
+      });
+
+    };
+
+
+  }]);
